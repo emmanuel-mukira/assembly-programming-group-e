2f718d048f74e808e0f2bc64ecdcdb5b224980a4
emmanuel-mukira <emmanuelgichigo@gmail.com>
2025-10-30 13:29:43 +0300

    c

---
+++ 08_file_handling/c_code/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Declare the external Assembly function
+extern uint64_t factorial(uint64_t n);
+
+int main(void) {
+    uint64_t n = 5;
+    uint64_t result = factorial(n);
+
+    printf("Factorial of %lu = %lu\n", n, result);
+    return 0;
+}
